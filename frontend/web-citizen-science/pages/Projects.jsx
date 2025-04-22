8a1b1fbeb78ac25c59525bf649aa3fed373b6bf2
Pavluck <sillsn@oregonstate.edu>
2025-04-22 16:11:34 -0700

    Working on the setup/logic for getting data from backend

---
+++ frontend/web-citizen-science/pages/Projects.jsx
@@ -0,0 +1,7 @@
+/* Tailors the data for the View Project Page on the Admin Website */
+
+/* ------------ Necessary Imports ------------*/
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import DataTable from '../components/DataTable';
