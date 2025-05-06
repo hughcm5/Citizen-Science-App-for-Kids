148ffa9a9ef9dea4aed988340c661587a5f626ec
JosephMesser13 <josephmesser13@gmail.com>
2025-05-05 17:17:17 -0700

    added classrooms URL

--- backend/api_gateway/api_gateway.py
+++ backend/api_gateway/api_gateway.py
@@ -33,6 +33,7 @@ SERVICE_URLS = {
     'students': os.getenv('STUDENTS_SERVICE_URL') or 'http://localhost:5003',
     'teachers': os.getenv('TEACHERS_SERVICE_URL') or 'http://localhost:5004',
     'csv': os.getenv('CSV_SERVICE_URL') or 'http://localhost:5005',
+    'classrooms': os.getenv('CLASSROOMS_SERVICE_URL') or 'http://localhost:5006',
 }
 
 
