37d31a8f3afd8d4a308560e89be4ba8c2613b3d1
Natasha Pavelek <sillsn@oregonstate.edu>
2025-05-26 00:13:20 -0700

    Implement update for AdminPage.jsx
    Also added some functionality to update changes on the front end while waiting for a data fetch.

--- frontend/web-citizen-science/src/Components/AdminPage.jsx
+++ frontend/web-citizen-science/src/Components/AdminPage.jsx
@@ -9,8 +9,9 @@ import Col from "react-bootstrap/Col";
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 
-/* ------------ Page Content  ------------*/
+/* ------------ Admin Table  ------------*/
 function Admin() {
+      /* Prepare CRUD on the frontend */
   const [admin_firstname, setadmin_firstname] = useState('');
   const [admin_lastname, setadmin_lastname] = useState('');
   const [email, setemail] = useState('');
@@ -20,6 +21,10 @@ function Admin() {
   const [loading, setLoading] = useState(true);
   const [retrieveError, setRetrieveError] = useState(null);
 
+  const [editedId, setEditedId] = useState(null);
+  const [editedAdminData, setEditedAdminData] = useState({});
+  
+/* ------------ Retrieve  ------------*/
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:5000/admins');
@@ -36,6 +41,7 @@ function Admin() {
     fetchData();
   }, []);
 
+/* ------------ Create  ------------*/
   const handleSubmit = (event) => {
     event.preventDefault();
     const admin_payload = {
@@ -61,16 +67,17 @@ function Admin() {
 
   const toHumanReadableDate = (backendDateStr) => {
     const date = new Date(backendDateStr);
-    // TODO: Format the string to remove timezone (not needed)
+    // Format the string to remove timezone 
     return date.toISOString().split('T')[0];
   }
 
+  /* ------------ Delete  ------------*/
   const deleteAdmin = async (id) => {
   try {
     const response = await axios.delete(`http://localhost:5000/admins/` + id.toString());
     
     console.log('Admin deleted successfully:', response.data);
-    // Handle successful deletion (todo: update/refresh table upon deletion)
+    // Handle successful deletion (update/refresh table upon deletion)
     fetchData();
   } catch (error) {
     console.error('Error deleting admin:', error);
@@ -78,25 +85,90 @@ function Admin() {
   }
 };
 
+  const onEditRow = (admin) => {
+    // enable the labels by setting an edited id to render the inputs
+    setEditedId(admin.admin_id);
+    setEditedAdminData(admin);
+  }
+
+  const onEditChange = (x) => {
+    const { name, value } = x.target;
+    setEditedAdminData({...editedAdminData, [name]: value});
+  }
+
+  const cancelEdit = () => {
+    setEditedId(null);
+    setEditedAdminData({});
+  }
+
+  const onSaveEdit = async () => {
+    if (!editedId) {
+      // If nothing was edited, do nothing
+      return;
+    }
+
+    // TODO: Maybe do some frontend validation to catch mistakes before sending to the backend?
+
+    const admin_payload = {
+      'admin_id' : editedAdminData.admin_id,
+      'admin_firstname' : editedAdminData.admin_firstname,
+      'admin_lastname' : editedAdminData.admin_lastname,
+      'email' : editedAdminData.email,
+      'role' : editedAdminData.role,
+      'oauth_id' : editedAdminData.oauth_id
+    };
+
+    // TODO: Change or disable the "save" button to say "saving" or similar so that there is feedback that saving is happening
+
+    try {
+      const response = await axios.put(`http://localhost:5000/admins/` + editedAdminData.admin_id.toString(), admin_payload);
+      console.log('Admin updated successfully: ', response.data);
+      // Handle successful update (update/refresh table upon deletion)
+
+      // Update the fields on the front end side to speed it up?
+      // iterate to find the admin with the same id
+      const admin_to_be_updated = adminData.find(obj => obj.admin_id === editedAdminData.admin_id);
+      if (admin_to_be_updated) {
+        admin_to_be_updated.admin_firstname = editedAdminData.admin_firstname;
+        admin_to_be_updated.admin_lastname = editedAdminData.admin_lastname;
+        admin_to_be_updated.email = editedAdminData.email;
+        admin_to_be_updated.role = editedAdminData.role;
+        admin_to_be_updated.oauth_id = editedAdminData.oauth_id;
+      }
+
+      fetchData();
+      // A bit of an oxymoron since the edit was already successful but this is to disable the edit fields
+      cancelEdit();
+      // TODO: have some kind of popup telling the user of the successful edit
+    } catch (error) {
+      console.error('Error updating admin:', error);
+      // Error handling
+      // TODO: Maybe autocancel or leave up at editing mode?
+      // TODO: have some kind of popup telling the user that something wrong happened
+    }
+  }
+
+/* ------------ Page Content  ------------*/
   return (
     <Container fluid>
       <Container className="content">
         <Row>
           <Col md={9}>
             <h1 style={{paddingBottom: '40px'}}>
-             The science projects are administered by educators, so children can meaningfully contribute and gain experience in scientific research
+             The science projects are administered by educators
             </h1>
             <h2>Current Admins:</h2>
-            <p></p>
+            <br />
             {
               // For debugging purposes only
               /*
               <pre>{JSON.stringify(adminData, null, 2)}</pre>
               */
             }
-            <table class="adminTable">
+            <table className="adminTable">
               <thead>
                   <tr>
+                    <th> </th>
                     <th>ID #</th>
                     <th>Classes</th>
                     <th>Last Name</th>
@@ -110,15 +182,49 @@ function Admin() {
                   </thead>
                   <tbody>
                   {adminData.map(admin => (
-                    <tr id={admin}>
+                    <tr key={admin.admin_id}>
+                      <td>
+                        {
+                          editedId === admin.admin_id
+                          ? (<>
+                              <button onClick={() => cancelEdit()}>Cancel</button>
+                              <button onClick={() => onSaveEdit()}>Save</button>
+                            </>)
+                          : (<button onClick={() => onEditRow(admin)}>Edit</button>)
+                        }
+                      </td>
                       <td>{admin.admin_id}</td>
                       <td>{admin.classrooms.length}</td>
-                      <td>{admin.admin_lastname}</td>
-                      <td>{admin.admin_firstname}</td>
-                      <td>{admin.role}</td>
+                      <td>
+                        {
+                          editedId === admin.admin_id
+                          ? (<input name="admin_lastname" type="text" value={editedAdminData.admin_lastname} onChange={onEditChange} />)
+                          : (admin.admin_lastname)
+                        }
+                      </td>
+                      <td>
+                        {
+                          editedId === admin.admin_id
+                          ? (<input name="admin_firstname" type="text" value={editedAdminData.admin_firstname} onChange={onEditChange} />)
+                          : (admin.admin_firstname)
+                        }
+                      </td>
+                      <td>
+                        {
+                          editedId === admin.admin_id
+                          ? (<input name="role" type="text" value={editedAdminData.role} onChange={onEditChange} />)
+                          : (admin.role)
+                        }
+                      </td>
                       <td>{toHumanReadableDate(admin.created_at)}</td>
                       <td>{toHumanReadableDate(admin.updated_at)}</td>
-                      <td>{admin.email}</td>
+                      <td>
+                        {
+                          editedId === admin.admin_id
+                          ? (<input name="email" type="text" value={editedAdminData.email} onChange={onEditChange} />)
+                          : (admin.email)
+                        }
+                      </td>
                       <td><button onClick={(event) => deleteAdmin(admin.admin_id)}>Delete</button></td>
                     </tr>
                   ))}
@@ -129,12 +235,12 @@ function Admin() {
               <p></p>
             <form onSubmit={handleSubmit}>
               <label>
-              Create a new Admin - Enter the Admin's Details: <br />
+              Create a new Admin - Enter the Admin's Details: </label><br />
               <input type="text" placeholder="First Name" value={admin_firstname} onChange={(e) => setadmin_firstname(e.target.value)} />
               <input type="text" placeholder="Last Name" value={admin_lastname} onChange={(e) => setadmin_lastname(e.target.value)} />
-              <input type="text" placeholder="Admin's Email" value={email} onChange={(e) => setemail(e.target.value)} />
-              <input type="text" placeholder="Admin's Role (teacher, principle, etc)" value={role} onChange={(e) => setrole(e.target.value)} />
-              </label> <br />
+              <input type="text" placeholder="Admin's Email" value={email} onChange={(e) => setemail(e.target.value)}/>
+              <input type="text" placeholder="Admin's Role (teacher, principle, etc)" value={role} onChange={(e) => setrole(e.target.value)}/>
+              <br />
               <Button variant="primary" type="submit">Submit</Button>
             </form>
             </Col>
