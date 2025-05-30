141633addcbfa1e80f33f40366fa11e8479c6aff
JosephMesser13 <josephmesser13@gmail.com>
2025-05-29 17:47:17 -0700

    I had a typo in this file and have been looking in the wrong spot for an hour :(

--- frontend/web-citizen-science/src/Components/AdminPage.jsx
+++ frontend/web-citizen-science/src/Components/AdminPage.jsx
@@ -22,7 +22,7 @@ function Admin() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://backend-dot-citizen-science-app-for-kids.uc.r.appspot.com/admins');
+      const response = await axios.get('https://backend-dot-citizen-science-app-for-kids.wn.r.appspot.com/admins');
       // const response = await axios.get('http://localhost:5000/admins');
       setAdminData(response.data);
       setLoading(false);
@@ -47,7 +47,7 @@ function Admin() {
     }
     console.log('admin:', Admin);
     axios
-      .post("https://backend-dot-citizen-science-app-for-kids.uc.r.appspot.com/admins", admin_payload)
+      .post("https://backend-dot-citizen-science-app-for-kids.wn.r.appspot.com/admins", admin_payload)
       // .post("http://localhost:5000/admins", admin_payload)
       .then((response) => {
         console.log('Admin creation successful');
@@ -69,7 +69,7 @@ function Admin() {
 
   const deleteAdmin = async (id) => {
   try {
-    const response = await axios.delete(`https://backend-dot-citizen-science-app-for-kids.uc.r.appspot.com/admins` + id.toString());
+    const response = await axios.delete(`https://backend-dot-citizen-science-app-for-kids.wn.r.appspot.com/admins` + id.toString());
     
     console.log('Admin deleted successfully:', response.data);
     // Handle successful deletion (todo: update/refresh table upon deletion)
