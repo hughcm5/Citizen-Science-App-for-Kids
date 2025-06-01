bb3ad02e438c99cba4c4aee13fd5728261ae5909
Natasha Pavelek <sillsn@oregonstate.edu>
2025-06-01 02:46:27 -0700

    Implemented Initial Login functionality
    Database Pages on the Admin Website now require a login session, otherwise, will be redirected to the login page.

---
+++ frontend/web-citizen-science/src/Components/PrivateRoute.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+
+    const [loggedIn, setLoggedIn] = useState(null);
+
+    useEffect(() => {
+        axios
+        .get(process.env.REACT_APP_BACKEND_GATEWAY_URL + '/session')
+        .then((response) => {
+            const session_data = response.data;
+            setLoggedIn(session_data.logged_in);
+        })
+        .catch((error) => {
+            console.error('Failed to get session from the backend:', error);
+        });
+    }, []);
+
+    const location = useLocation();
+
+    if (loggedIn === null) {
+        return <div>Please wait ...</div>
+    }
+
+    if (loggedIn === false) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    
+    return children;
+};
+
+export default PrivateRoute;
